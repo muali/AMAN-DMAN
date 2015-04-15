488cc62a2fb1293fff9911eb05762958f732a494
Maxim Moskvitin <m-moskvitin92@yandex.ru>
2015-04-15 14:54:06 +0300

    Estimator

--- AMAN/schedulers.cpp
+++ AMAN/schedulers.cpp
@@ -1,25 +1,48 @@
 #include "schedulers.h"
 
-//-------------------------------------------------------------------------------------
+#include <algorithm>
 
-schedule greedy_scheduler::do_scheduling(const input_data& data, sequence seq) const
+#include <boost\date_time\posix_time\posix_time.hpp>
+
+namespace AMAN
 {
-    schedule result;
-    uint64_t last_landing = 0;
-    for (aircraft& current : seq)
+    estimator::estimator(std::shared_ptr<scheduler> p_scheduler)
+        : p_scheduler_(p_scheduler)
     {
-        uint64_t separation;
-        if (result.empty())
+
+    }
+
+    double estimator::estimate(const input_data& data, std::vector<aircraft> sequence) const
+    {
+        schedule schedule_result = p_scheduler_->do_scheduling(data, sequence);
+        double total_cost = 0.;
+        for (aircraft& item : sequence)
         {
-            separation = 0;
+            total_cost += item.estimate_cost(schedule_result.get(item));
         }
-        else
+        return total_cost;
+    }
+
+    schedule greedy_scheduler::do_scheduling(const input_data& data, std::vector<aircraft> sequence) const
+    {
+        using namespace std;
+        using namespace boost::posix_time;
+        schedule result;
+        ptime last_landing(data.get_start_time());
+        for (size_t i = 0; i < sequence.size(); ++i)
         {
-            id_type last_id = result.rbegin()->first;
-            separation = data.separations[last_id][current.id];
+            time_duration separation;
+            if (i == 0)
+            {
+                separation = seconds(0);
+            }
+            else
+            {
+                separation = data.get_separation(sequence[i - 1], sequence[i]);
+            }
+            ptime landing_time(std::max(sequence[i].get_min_time(), last_landing + separation));
+            result.set(sequence[i], landing_time);
         }
-        uint64_t landing_time = std::max(current.earleast_landing, last_landing + separation);
-        result.push_back(std::make_pair(current.id, landing_time));
-    }                                                              
-    return result;
+        return result;
+    }
 }
--- AMAN/schedulers.h
+++ AMAN/schedulers.h
@@ -1,40 +1,52 @@
 #pragma once
 
-#include "data_types.h"
+#include "input_data.h"
+#include "schedule.h"
 
-//-------------------------------------------------------------------------------------
+#include <vector>
+#include <memory>
 
-struct scheduler
+namespace AMAN
 {
-    virtual schedule do_scheduling(const input_data&, sequence) const = 0;
-    virtual ~scheduler() = default;
-};
+    //-------------------------------------------------------------------------------------
 
-//-------------------------------------------------------------------------------------
+    struct scheduler
+    {
+        virtual schedule do_scheduling(const input_data&, std::vector<aircraft> sequence) const = 0;
+        virtual ~scheduler() = default;
+    };
 
-struct greedy_scheduler
-    :   scheduler
-{
-    greedy_scheduler() = default;
-    ~greedy_scheduler() = default;
+    //-------------------------------------------------------------------------------------
 
-    schedule do_scheduling(const input_data&, sequence) const override;
-};
+    struct estimator
+    {
+        estimator(std::shared_ptr<scheduler>);
+        double estimate(const input_data&, std::vector<aircraft> sequence) const;
+    private:
+        std::shared_ptr<scheduler> p_scheduler_;
+    };
 
-//-------------------------------------------------------------------------------------
+    struct greedy_scheduler
+        : scheduler
+    {
+        schedule do_scheduling(const input_data&, std::vector<aircraft> sequence) const override;
+    };
 
-struct dp_scheduler
-    :   scheduler
-{
-    dp_scheduler(uint64_t time_sampling);
-    ~dp_scheduler();
+    //-------------------------------------------------------------------------------------
 
-    schedule do_scheduling(const input_data&, sequence) const override;
-};
+    struct dp_scheduler
+        : scheduler
+    {
+        dp_scheduler(uint64_t time_sampling);
+        
+        schedule do_scheduling(const input_data&, std::vector<aircraft> sequence) const override;
+    };
 
-//-------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------
 
-struct linear_scheduler
-{
+    struct linear_scheduler
+    {
 
-};
+    };
+
+} // AMAN
